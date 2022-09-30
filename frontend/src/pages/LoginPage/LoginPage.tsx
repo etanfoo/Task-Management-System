8ee9020cc1880a69ef80345390b8dbb175c556c0
Terence <z5311419@student.unsw.edu.au>
2022-09-30 17:03:05 +1000

    Started implementing basic features of login page

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,8 +1,26 @@
 import { LoginPageContainer } from "./style";
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
   return (
     <LoginPageContainer>
-      this is the login page
+      Login to TaskHub
+      <Stack spacing={2} direction="column">
+        <TextField
+          id="login-email"
+          placeholder="Email"
+        />
+        <TextField
+          id="login-password"
+          placeholder="Password"
+        />
+      </Stack>
+      New to TaskHub? 
+      <Link to="/signup">Sign up</Link>
+      <Button variant="contained">Login</Button>
     </LoginPageContainer>
   )
 };
--- frontend/src/pages/LoginPage/style.ts
+++ frontend/src/pages/LoginPage/style.ts
@@ -1,5 +1,9 @@
 import { styled } from "@mui/system";
 
 export const LoginPageContainer = styled('div')`
-  // TODO: add css here
+  margin: auto;
+  width: 35%;  
+  // display: flex;
+  // justify-content: center;
+  // flex-direction: column;
 `;
