c1d0798ab8e80a09f0b452f6d8499b4b91efe4a0
aminbeigi <me@aminbeigi.com>
2022-10-02 23:57:30 +1100

    Clean up code

--- backend/src/test/java/com/redlions/backend/ProfileTests.java
+++ backend/src/test/java/com/redlions/backend/ProfileTests.java
@@ -1,14 +1,20 @@
 package com.redlions.backend;
 
+import com.redlions.backend.entity.Profile;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,13 +28,14 @@ public class ProfileTests {
     public void GetUserWithInvalidId_Throws400() throws IOException {
         String id = "9929";
         HttpUriRequest request = new HttpGet(URL + "/" + id);
-        HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.BAD_REQUEST.value());
+        HttpResponse response = HttpClientBuilder.create().build().execute( request );
+        assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.BAD_REQUEST.value());
 
         String id2 = "horse";
         request = new HttpGet(URL + "/" + id2);
-        httpResponse = HttpClientBuilder.create().build().execute( request );
-        assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.BAD_REQUEST.value());
+        response = HttpClientBuilder.create().build().execute( request );
+
+        assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.BAD_REQUEST.value());
     }
 
     @Test
@@ -40,14 +47,12 @@ public class ProfileTests {
     }
 
     //@Test
-    //public void UpdateUserWithAboutMeAbove300Characters_Throws400() throws IOException, URISyntaxException {
-    //    String aboutMe = "...";
-    //
-    //    URIBuilder builder = new URIBuilder(URL + "/1" + "1");
-    //    builder.setParameter("aboutMe", aboutMe);
+    //public void CreateUser() throws IOException, URISyntaxException {
+    //    Profile profile = new Profile(10L, "batman", "profile1@email.com", "password", 1L, 1L, "aboutme", "temp".getBytes());
+    //    URIBuilder builder = new URIBuilder(URL);
+    //    builder.setParameter("profile", profile);
     //    URI uri = builder.build();
-    //    HttpUriRequest request = new HttpPut(uri);
-    //    String bob = request.getURI().toString();
+    //    HttpUriRequest request = new HttpPost(uri);
     //    HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
     //    assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.BAD_REQUEST.value());
     //}
