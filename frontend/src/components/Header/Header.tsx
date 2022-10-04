cd8a066fd3c2341df23f3389570d002e63e6b53f
sunny_thinkpad <sunnyw987@gmail.com>
2022-10-04 16:36:21 +1100

    feat(frontend): implement basic header

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,85 @@
+import { HeaderContainer, LoginLink, SignUpButton, Logo } from "./style";
+import LogoIcon from "../../assets/COMP3900-Logo.png";
+import ProfileIcon from "../../assets/profile.png";
+import { Menu, IconButton, MenuItem } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const handleMenuOpen = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(e.currentTarget);
+    setMenuOpen(true);
+  }
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    setMenuOpen(false);
+  }
+
+  const handleLogout = () => {
+    sessionStorage.removeItem(process.env.REACT_APP_TOKEN!);
+    navigate('/');
+  }
+
+  const handleLogoClick = () => {
+    if (sessionStorage.getItem(process.env.REACT_APP_TOKEN!)) {
+      navigate('/dashboard');
+    } else {
+      navigate('/');
+    }
+  };
+
+  return (
+    <HeaderContainer>
+      <Logo src={LogoIcon} alt='logo' onClick={handleLogoClick}/>
+      {sessionStorage.getItem(process.env.REACT_APP_TOKEN!)
+        ? 
+          (
+            <>
+              <IconButton
+                onClick={handleMenuOpen}
+                sx={{ marginLeft: "auto", height: "3rem" }}
+              >
+                <img src={ProfileIcon} style={{ height: '2.5rem' }} alt="profile" />
+              </IconButton>
+              <Menu
+                open={menuOpen}
+                anchorEl={anchorEl}
+                onClose={handleMenuClose}
+              >
+                {/* todo: update with logos */}
+                <MenuItem onClick={() => navigate('/profile')}>
+                  Profile 
+                </MenuItem>
+                {/* todo: update with friends  */}
+                <MenuItem onClick={() => navigate('/friends')}>
+                  Look for a friend 
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                  Logout 
+                </MenuItem>
+              </Menu>
+            </>
+          )
+        : (
+          <>
+            <LoginLink href='/login'>
+              Login
+            </LoginLink>
+            <SignUpButton variant='contained' onClick={() => navigate('/signup')}>
+              Sign up
+            </SignUpButton>
+          </>
+        )
+      }
+
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/Header/style.ts
@@ -0,0 +1,37 @@
+import { Button } from "@mui/material";
+import { styled } from "@mui/system";
+import { Palette } from "../Palette";
+
+export const HeaderContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  padding: 0 2rem;
+  height: 5rem;
+  margin-bottom: 1rem;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const Logo = styled('img')`
+  height: 4rem;
+  cursor: pointer;
+`;
+
+export const SignUpButton = styled(Button)`
+  background-color: ${Palette.mainTeal};
+  &:hover {
+    background-color: ${Palette.darkTeal};
+  }
+`;
+
+export const LoginLink = styled('a')`
+  margin: 0 1.5rem 0 auto;
+  text-decoration: none;
+  color: black;
+  font-size: 1rem;
+  transition: all 0.3s ease 0s;
+  &:hover {
+    color: ${Palette.mainTeal};
+  }
+`;
