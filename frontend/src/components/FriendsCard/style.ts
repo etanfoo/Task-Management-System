2b14f1cfc39c88933f919ae9df6d518fa8b1d1b7
sunny_thinkpad <sunnyw987@gmail.com>
2022-10-08 21:25:05 +1100

    feat(frontend): implement friends card

---
+++ frontend/src/components/FriendsCard/style.ts
@@ -0,0 +1,44 @@
+import { Avatar } from "@mui/material";
+import { styled } from "@mui/system";
+import { Palette } from "../Palette";
+
+export const FriendsCardContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  cursor: pointer;
+  border-radius: 0.5rem;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  padding: 1rem;
+
+  transition: transform .2s;
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  > img {
+    height: 3.125rem;
+    margin-right: 1rem;
+    border-radius: 5rem;
+  };
+`;
+
+export const StyledAvatar = styled(Avatar)`
+  margin-right: 1rem;
+  background-color: ${Palette.mainTeal};
+`;
+
+export const DetailsContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+
+  > h3 {
+    margin: 0;
+    font-weight: normal;
+  };
+
+  > p {
+    margin: 0;
+    color: ${Palette.thGray}
+  };
+`;
