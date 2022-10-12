894af2e00cbea49258a744d6957d32ee3001fe13
Terence <z5311419@student.unsw.edu.au>
2022-10-12 22:08:25 +1100

    Fixed minor bug with missing angle bracket

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -78,143 +78,144 @@ const ProfilePage = () => {
   }, [profileId]);
 
   return(
-    {isLoading 
-      ? <LoadingOverlay isOpen={isLoading}/>
-      : (
-        <ProfilePageContainer>
-          <Header />
-          <TopContainer>
-            {pageState === 'view'
-              ? (
-                profileDetails.profilePicture
-                  ? <img src={profileDetails.profilePicture} alt='user avatar'/>
-                  : (
-                    <StyledAvatar>
-                      {profileDetails.name.split(' ').length >= 2
-                      ? profileDetails.name.split(' ')[0][0] + profileDetails.name.split(' ')[1][0]
-                      : profileDetails.name.split(' ')[0][0]
-                      }
-                    </StyledAvatar>
-                  )
-              ) : (
-                <StyledLabel>
-                  {!!updatedProfileDetails.profilePicture
-                    ? (
-                      <img src={updatedProfileDetails.profilePicture} alt='user avatar'/>
-                    ) : (
-                      profileDetails.profilePicture
-                        ? <img src={profileDetails.profilePicture} alt='user avatar'/>
-                        : <EmptyAvatar />
-                    )
-                  }
-                  <input
-                    type='file'
-                    accept='.jpg, .png'
-                    onChange={updateLogo}
-                  />              
-                </StyledLabel>
-              )
-            }
-            <DetailsContainer>
-              {pageState === 'view'
-                ? (
-                  <h1>{`${profileDetails?.name} (Busyness - 20%)`}</h1>
-                ) : (
-                    <TextField
-                      placeholder={profileDetails.name}
-                      onChange={(e) => setUpdatedProfileDetails({ ...updatedProfileDetails, name: e.target.value })}
-                    />
-                  )
-                }
-              <p>{profileDetails.email}</p>
-            </DetailsContainer>
-            {isSelfProfile ?
-              (
-                <IconContainer>
-                  {/* todo: update url of info page? */}
-                  {pageState === 'view'
-                    ? (
-                      <>
-                        <img src={InfoIcon} onClick={() => navigate('/info')} alt='info icon' />
-                        <img src={EditIcon} onClick={() => setPageState('edit')} alt='edit icon' />
-                      </>
-                    ) : (
-                      <>
-                        <CancelButton variant='contained' onClick={cancelEditProfile}>Cancel</CancelButton>
-                        <UpdateButton variant='contained' onClick={() => updateProfile()}>Update</UpdateButton>
-                      </>
-                    )
-                  }
-                </IconContainer>
-              ) : null
-            }
-          </TopContainer>
-          <BodyContainer>
-            <LeftContainer>
-              <AboutMeContainer>
+    <>
+      {isLoading 
+        ? <LoadingOverlay isOpen={isLoading}/>
+        : (
+            <ProfilePageContainer>
+              <Header />
+              <TopContainer>
                 {pageState === 'view'
                   ? (
-                    profileDetails.aboutMe ||
-                      <p>
-                        {isSelfProfile
-                          ? "Tell us a little about yourself..."
-                          : "This user has yet to provide a bio."
-                        }
-                      </p>
-                  ): (
-                    <TextField
-                      multiline
-                      rows={4}
-                      placeholder="Tell us a little about yourself..."
-                      onChange={(e) => setUpdatedProfileDetails({...updatedProfileDetails, aboutMe: e.target.value })}
-                      sx={{ width: '100%' }}
-                    />
+                    profileDetails.profilePicture
+                      ? <img src={profileDetails.profilePicture} alt='user avatar'/>
+                      : (
+                        <StyledAvatar>
+                          {profileDetails.name.split(' ').length >= 2
+                          ? profileDetails.name.split(' ')[0][0] + profileDetails.name.split(' ')[1][0]
+                          : profileDetails.name.split(' ')[0][0]
+                          }
+                        </StyledAvatar>
+                      )
+                  ) : (
+                    <StyledLabel>
+                      {!!updatedProfileDetails.profilePicture
+                        ? (
+                          <img src={updatedProfileDetails.profilePicture} alt='user avatar'/>
+                        ) : (
+                          profileDetails.profilePicture
+                            ? <img src={profileDetails.profilePicture} alt='user avatar'/>
+                            : <EmptyAvatar />
+                        )
+                      }
+                      <input
+                        type='file'
+                        accept='.jpg, .png'
+                        onChange={updateLogo}
+                      />              
+                    </StyledLabel>
                   )
                 }
-              </AboutMeContainer>
-              <TasksContainer>
-                <h2>Assigned tasks</h2>
-                <LabelContainer>
-                  <p>ID</p>
-                  <p>Title</p>
-                  <p>Deadline</p>
-                  <p>Status</p>
-                </LabelContainer>
-                <OverflowContainer>
-                  {/* todo: replace with real data returned from api */}
-                  {MockTasks.map((task) => (
-                    <TaskCard
-                      key={task.taskId}
-                      taskId={task.taskId}
-                      title={task.title}
-                      deadline={task.deadline}
-                      status={task.status}
-                    />
-                  ))}
-                </OverflowContainer>
-              </TasksContainer>
-            </LeftContainer>
-            <FriendsContainer>
-              <h2>Friends</h2>
-              <OverflowContainer>
-                {/* todo: replace with real data returned from api */}
-                {MockFriends.map((friend) => (
-                  <FriendsCard
-                    key={friend.profileId}
-                    profileId={friend.profileId}
-                    name={friend.name}
-                    email={friend.email}
-                    imageURL={friend.imageURL}
-                  />
-                ))}
-              </OverflowContainer>
-            </FriendsContainer>
-          </BodyContainer>
-          <Footer />
-        </ProfilePageContainer>
-        )
-      }
-    <>  
+                <DetailsContainer>
+                  {pageState === 'view'
+                    ? (
+                      <h1>{`${profileDetails?.name} (Busyness - 20%)`}</h1>
+                    ) : (
+                        <TextField
+                          placeholder={profileDetails.name}
+                          onChange={(e) => setUpdatedProfileDetails({ ...updatedProfileDetails, name: e.target.value })}
+                        />
+                      )
+                    }
+                  <p>{profileDetails.email}</p>
+                </DetailsContainer>
+                {isSelfProfile ?
+                  (
+                    <IconContainer>
+                      {/* todo: update url of info page? */}
+                      {pageState === 'view'
+                        ? (
+                          <>
+                            <img src={InfoIcon} onClick={() => navigate('/info')} alt='info icon' />
+                            <img src={EditIcon} onClick={() => setPageState('edit')} alt='edit icon' />
+                          </>
+                        ) : (
+                          <>
+                            <CancelButton variant='contained' onClick={cancelEditProfile}>Cancel</CancelButton>
+                            <UpdateButton variant='contained' onClick={() => updateProfile()}>Update</UpdateButton>
+                          </>
+                        )
+                      }
+                    </IconContainer>
+                  ) : null
+                }
+              </TopContainer>
+              <BodyContainer>
+                <LeftContainer>
+                  <AboutMeContainer>
+                    {pageState === 'view'
+                      ? (
+                        profileDetails.aboutMe ||
+                          <p>
+                            {isSelfProfile
+                              ? "Tell us a little about yourself..."
+                              : "This user has yet to provide a bio."
+                            }
+                          </p>
+                      ): (
+                        <TextField
+                          multiline
+                          rows={4}
+                          placeholder="Tell us a little about yourself..."
+                          onChange={(e) => setUpdatedProfileDetails({...updatedProfileDetails, aboutMe: e.target.value })}
+                          sx={{ width: '100%' }}
+                        />
+                      )
+                    }
+                  </AboutMeContainer>
+                  <TasksContainer>
+                    <h2>Assigned tasks</h2>
+                    <LabelContainer>
+                      <p>ID</p>
+                      <p>Title</p>
+                      <p>Deadline</p>
+                      <p>Status</p>
+                    </LabelContainer>
+                    <OverflowContainer>
+                      {/* todo: replace with real data returned from api */}
+                      {MockTasks.map((task) => (
+                        <TaskCard
+                          key={task.taskId}
+                          taskId={task.taskId}
+                          title={task.title}
+                          deadline={task.deadline}
+                          status={task.status}
+                        />
+                      ))}
+                    </OverflowContainer>
+                  </TasksContainer>
+                </LeftContainer>
+                <FriendsContainer>
+                  <h2>Friends</h2>
+                  <OverflowContainer>
+                    {/* todo: replace with real data returned from api */}
+                    {MockFriends.map((friend) => (
+                      <FriendsCard
+                        key={friend.profileId}
+                        profileId={friend.profileId}
+                        name={friend.name}
+                        email={friend.email}
+                        imageURL={friend.imageURL}
+                      />
+                    ))}
+                  </OverflowContainer>
+                </FriendsContainer>
+              </BodyContainer>
+              <Footer />
+            </ProfilePageContainer>
+          )
+        }
+    </>  
   );
 }
 
