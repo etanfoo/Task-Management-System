07b0a2202c96675ef66830b61bb530bcb261dd8c
Etan Foo <etanfoo@gmail.com>
2022-10-16 15:10:58 +1100

    cleanup and util class

--- backend/src/main/java/com/redlions/backend/BackendApplication.java
+++ backend/src/main/java/com/redlions/backend/BackendApplication.java
@@ -1,14 +1,11 @@
 package com.redlions.backend;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.redlions.backend.entity.Profile;
-import com.redlions.backend.service.ProfileService;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
