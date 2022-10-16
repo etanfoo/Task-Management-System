898623b4d579a93bba4027fe1644a624f405ac83
Etan Foo <etanfoo@gmail.com>
2022-10-16 13:59:52 +1100

    set up task function stubs

--- backend/src/main/java/com/redlions/backend/service/TaskService.java
+++ backend/src/main/java/com/redlions/backend/service/TaskService.java
@@ -1,5 +1,16 @@
 package com.redlions.backend.service;
 
+import java.util.List;
+import java.util.Set;
+
+import com.redlions.backend.entity.Project;
+import com.redlions.backend.entity.Task;
+
 public interface TaskService {
-    
+    Task create(Task task, Project project, Long profileId);
+    Task update(Task task, Project project, Long profileId);
+    Task getTask(Long id);
+    void delete(Long id);
+    void removeProfilesFromTask(Long taskId, Long profileId, Set<Long> profileIds);
+    List<Task> getAssociatedTasks(Long profileId);
 }
--- backend/src/main/java/com/redlions/backend/service/TaskServiceImplementation.java
+++ backend/src/main/java/com/redlions/backend/service/TaskServiceImplementation.java
@@ -1,5 +1,33 @@
 package com.redlions.backend.service;
 
+import java.util.List;
+import java.util.Set;
+
+import com.redlions.backend.entity.Project;
+import com.redlions.backend.entity.Task;
+
 public class TaskServiceImplementation implements TaskService {
-    
+    public Task create(Task task, Project project, Long profileId) {
+        return null;
+    }
+
+    public Task update(Task task, Project project, Long profileId) {
+        return null;
+    }
+
+    public Task getTask(Long id) {
+        return null;
+    }
+
+    public void delete(Long id) {
+
+    }
+
+    public void removeProfilesFromTask(Long taskId, Long profileId, Set<Long> profileIds) {
+
+    }
+
+    public List<Task> getAssociatedTasks(Long profileId) {
+        return null;
+    }
 }
