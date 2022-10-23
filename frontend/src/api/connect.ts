5370b8d91632794432f5a1f3684d4932e3de3294
sunny_thinkpad <sunnyw987@gmail.com>
2022-10-23 22:54:39 +1100

    fix(frontend): accept and reject api call type

--- frontend/src/api/connect.ts
+++ frontend/src/api/connect.ts
@@ -29,7 +29,7 @@ export const requestConnection = async (
 
 export const acceptConnection = async (userId: number, requestorId: number) => {
   try {
-    await axios.put(`${process.env.REACT_APP_API_URL}/api/v1/profile/accept/${userId}/${requestorId}`, {
+    await axios.post(`${process.env.REACT_APP_API_URL}/api/v1/profile/accept/${userId}/${requestorId}`, {
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem(process.env.REACT_APP_TOKEN!)}`
       }
@@ -41,7 +41,7 @@ export const acceptConnection = async (userId: number, requestorId: number) => {
 
 export const rejectConnection = async (userId: number, requestorId: number) => {
   try {
-    await axios.put(`${process.env.REACT_APP_API_URL}/api/v1/profile/reject/${userId}/${requestorId}`, {
+    await axios.post(`${process.env.REACT_APP_API_URL}/api/v1/profile/reject/${userId}/${requestorId}`, {
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem(process.env.REACT_APP_TOKEN!)}`
       }
