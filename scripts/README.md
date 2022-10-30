83eccbede94f77901258b678a61376de366a4607
aminbeigi <me@aminbeigi.com>
2022-10-30 11:30:29 +1100

    Update script

--- scripts/README.md
+++ scripts/README.md
@@ -1,6 +1,5 @@
 # Scripts
 ## `create_users.py`
-Takes in one arguement that is the amount of users to be created.  
 Run following in `/scripts`.  
-Example usage: `python3 create_users.py 50`
+Example usage: `python3 create_users.py`
 
--- scripts/create_users.py
+++ scripts/create_users.py
@@ -1,36 +1,32 @@
 import requests
 import json
-import sys
 
 from http import HTTPStatus
 
 API_PORT = 7777
-URL = f'http://localhost:{API_PORT}/api/v1/profile/signup'
-JSON_FILE_PATH = 'mock_signup_data.json'
-MAX_USERS_TO_CREATE = 1000
-MIN_USERS_TO_CREATE = 1
+BASE_URL = f'http://localhost:{API_PORT}/api/v1'
+SIGNUP_ENDPOINT = f'{BASE_URL}/profile/signup'
+JSON_FILE_PATH = 'mock_user_data.json'
+NUMBER_OF_USERS = 50
+USER_ABOUTME_CHARACTER_LIMIT = 250
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print(f"Usage: python3 {sys.argv[0]} [number]")
-        exit(1)
-    number_of_users_to_create = int(sys.argv[1])
-    if number_of_users_to_create > MAX_USERS_TO_CREATE:
-        print(f"Maximum users to create is {MAX_USERS_TO_CREATE}")
-        exit(1)
-    elif number_of_users_to_create < MIN_USERS_TO_CREATE:
-        print(f"Minimum users to create is {MIN_USERS_TO_CREATE}")
-        exit(1)
 
     with open(JSON_FILE_PATH) as f:
         users = json.load(f)
 
-    for i in range(number_of_users_to_create):
+    for i in range(NUMBER_OF_USERS):
         user = users[i]
-        payload = { "name": user.get("name"), "email": user.get("email"), "password": user.get("password")}
-        response = requests.post(URL, json=payload)
+        payload = { 
+            "name": user.get("name"),
+            "email": user.get("email"),
+            "password": user.get("password"),
+            "aboutMe": user.get("aboutMe")[:USER_ABOUTME_CHARACTER_LIMIT],
+            "happiness": user.get("happiness")
+            }
+        response = requests.post(SIGNUP_ENDPOINT, json=payload)
         if response.status_code != HTTPStatus.OK:
-            print(f"Error! Sent {i} POST requests to {URL} before exiting...")
+            print(f"Error {response.status_code}! Sent {i} POST requests to {SIGNUP_ENDPOINT} before exiting...")
             exit(1)
 
-    print(f"Success! Sent {i+1} POST requests to {URL}.")
+    print(f"Success! Sent {i+1} POST requests to {SIGNUP_ENDPOINT}.")
