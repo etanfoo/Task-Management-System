a57711912a69d506f5f5dcdbb0ae1f5129b0c3cd
aminbeigi <me@aminbeigi.com>
2022-10-30 19:28:25 +1100

    Move around constants

--- scripts/create_users.py
+++ scripts/create_users.py
@@ -7,12 +7,12 @@ API_PORT = 7777
 BASE_URL = f'http://localhost:{API_PORT}/api/v1'
 SIGNUP_ENDPOINT = f'{BASE_URL}/profile/signup'
 PROFILE_ENDPOINT = f'{BASE_URL}/profile'
+PROJECT_ENDPOINT = f'{BASE_URL}/project'
 JSON_FILE_PATH = 'mock_user_data.json'
 NUMBER_OF_USERS = 50
 USER_ABOUTME_CHARACTER_LIMIT = 250
-
-PROJECT_ENDPOINT = f'{BASE_URL}/project'
 REACT_APP_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJvdHJhaGVhcjBAbGl2ZWludGVybmV0LnJ1IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo3Nzc3L2FwaS92MS9wcm9maWxlL2xvZ2luIiwiZXhwIjoxNjY3MTk4NzgwfQ.INSe7YuZVsOMQYcxx_9LYFn65wflghbQY5RFr7hDtKU"
+
 
 """
 Create `NUMBER_OF_USERS` number of users.
