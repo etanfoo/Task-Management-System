c7db33ec375efe7b413587697dc6c02d7968f92d
aminbeigi <me@aminbeigi.com>
2022-10-30 19:01:15 +1100

    Attempt to add projects via script

--- scripts/create_users.py
+++ scripts/create_users.py
@@ -10,8 +10,11 @@ JSON_FILE_PATH = 'mock_user_data.json'
 NUMBER_OF_USERS = 50
 USER_ABOUTME_CHARACTER_LIMIT = 250
 
-if __name__ == '__main__':
+PROJECT_ENDPOINT = f'{BASE_URL}/project'
+REACT_APP_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJvdHJhaGVhcjBAbGl2ZWludGVybmV0LnJ1IiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo3Nzc3L2FwaS92MS9wcm9maWxlL2xvZ2luIiwiZXhwIjoxNjY3MTk4NzgwfQ.INSe7YuZVsOMQYcxx_9LYFn65wflghbQY5RFr7hDtKU"
 
+
+def main():
     with open(JSON_FILE_PATH) as f:
         users = json.load(f)
 
@@ -30,3 +33,22 @@ if __name__ == '__main__':
             exit(1)
 
     print(f"Success! Sent {i+1} POST requests to {SIGNUP_ENDPOINT}.")
+
+if __name__ == '__main__':
+    main()
+
+    #payload = { 
+    #    "profileId": 11,
+    #    "profiles": [1, 2, 3, 4, 5],
+    #    "project": {
+    #        "title": "project title",
+    #        "description": "project description",
+    #        "profiles": []
+    #    }
+    #}
+    #headers = {
+    #    "Authorization": REACT_APP_TOKEN
+    #}
+
+    #response = requests.post(PROJECT_ENDPOINT, json=payload, headers=headers)
+    #print(response)
