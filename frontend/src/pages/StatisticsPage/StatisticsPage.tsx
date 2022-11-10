c6cf1c1cf9448a461abe45382652a46d074d9cab
sunny_thinkpad <sunnyw987@gmail.com>
2022-11-10 11:44:18 +1100

    feat(frontend): implement statistics page layout

--- frontend/src/pages/StatisticsPage/StatisticsPage.tsx
+++ frontend/src/pages/StatisticsPage/StatisticsPage.tsx
@@ -1,132 +1,184 @@
+// import { getProject } from "../../api/project";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { SectionContainer, StatisticsPageContainer, GraphWrapper } from "./style";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import { Divider } from "@mui/material";
 import {
   Chart as ChartJS,
+  ArcElement,
   CategoryScale,
   LinearScale,
   BarElement,
   Title,
   Tooltip,
   Legend,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-import { getProject } from "../../api/project";
-import { useEffect, useState } from "react";
-import { IProfile } from "../../interfaces/api-response";
-import { StatisticsPageContainer } from "./style";
+} from 'chart.js';
+import { Bar, Pie } from 'react-chartjs-2';
 
 ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
   CategoryScale,
   LinearScale,
   BarElement,
   Title,
-  Tooltip,
-  Legend
 );
 
-const HappinessTrackerGraph = () => {
-  const HAPPINESS_TRACKER_BACKGROUND_RED = "rgb(255, 99, 132, 0.2)";
-  const HAPPINESS_TRACKER_BACKGROUND_ORANGE = "rgb(255, 159, 64, 0.2)";
-  const HAPPINESS_TRACKER_BACKGROUND_PURPLE = "rgb(153, 102, 255, 0.2)";
-  const HAPPINESS_TRACKER_BACKGROUND_YELLOW = "rgb(255, 205, 86, 0.2)";
-  const HAPPINESS_TRACKER_BACKGROUND_GREEN = "rgb(75, 192, 192, 0.2)";
-
-  const HAPPINESS_TRACKER_BORDER_RED = "rgb(255, 99, 132)";
-  const HAPPINESS_TRACKER_BORDER_ORANGE = "rgb(255, 159, 64)";
-  const HAPPINESS_TRACKER_BORDER_PURPLE = "rgb(153, 102, 255)";
-  const HAPPINESS_TRACKER_BORDER_YELLOW = "rgb(255, 205, 86)";
-  const HAPPINESS_TRACKER_BORDER_GREEN = "rgb(75, 192, 192)";
-
-  const EMPTY_HAPPINESS_TRACKER_DATA = [0, 0, 0, 0, 0];
-
-  const { projectId } = useParams();
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [happinessTrackerData, setHappinessTrackerData] = useState(
-    EMPTY_HAPPINESS_TRACKER_DATA
-  );
-
-  const options = {
-    scales: {
-      y: {
-        ticks: {
-          stepSize: 1,
-        },
-      },
-    },
-
-    responsive: true,
-    plugins: {
-      legend: {
-        display: false,
-      },
-      title: {
-        display: true,
-        text: "Happiness Tracker",
-      },
-    },
-  };
+const StatisticsPage = () => {
 
   const data = {
-    labels: ["Stressed", "Worried", "Neutral", "Comfortable", "Happy"],
+    labels: ['Stressed', 'Worried', 'Neutral', 'Comfortable', 'Happy'],
     datasets: [
       {
-        label: "Happiness",
-        data: happinessTrackerData,
+        label: '# of Votes',
+        data: [12, 19, 3, 2, 3],
         backgroundColor: [
-          HAPPINESS_TRACKER_BACKGROUND_RED,
-          HAPPINESS_TRACKER_BACKGROUND_ORANGE,
-          HAPPINESS_TRACKER_BACKGROUND_PURPLE,
-          HAPPINESS_TRACKER_BACKGROUND_YELLOW,
-          HAPPINESS_TRACKER_BACKGROUND_GREEN,
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
         ],
         borderColor: [
-          HAPPINESS_TRACKER_BORDER_RED,
-          HAPPINESS_TRACKER_BORDER_ORANGE,
-          HAPPINESS_TRACKER_BORDER_PURPLE,
-          HAPPINESS_TRACKER_BORDER_YELLOW,
-          HAPPINESS_TRACKER_BORDER_GREEN,
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
         ],
         borderWidth: 1,
       },
     ],
   };
 
-  const loadProject = async () => {
-    try {
-      const response = await getProject(projectId!);
-
-      let tmpHappinessTrackerData = EMPTY_HAPPINESS_TRACKER_DATA;
-      response.profiles.map(
-        (profile: IProfile) => ++tmpHappinessTrackerData[profile.happiness!]
-      );
-      setHappinessTrackerData(tmpHappinessTrackerData);
-      setIsLoading(false);
-    } catch (err: any) {
-      console.log(err);
-    }
+  const MOCKBAROPTIONS = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
   };
+  // const HAPPINESS_TRACKER_BACKGROUND_RED = "rgb(255, 99, 132, 0.2)";
+  // const HAPPINESS_TRACKER_BACKGROUND_ORANGE = "rgb(255, 159, 64, 0.2)";
+  // const HAPPINESS_TRACKER_BACKGROUND_PURPLE = "rgb(153, 102, 255, 0.2)";
+  // const HAPPINESS_TRACKER_BACKGROUND_YELLOW = "rgb(255, 205, 86, 0.2)";
+  // const HAPPINESS_TRACKER_BACKGROUND_GREEN = "rgb(75, 192, 192, 0.2)";
 
-  useEffect(() => {
-    loadProject();
-    // eslint-disable-next-line
-  }, []);
+  // const HAPPINESS_TRACKER_BORDER_RED = "rgb(255, 99, 132)";
+  // const HAPPINESS_TRACKER_BORDER_ORANGE = "rgb(255, 159, 64)";
+  // const HAPPINESS_TRACKER_BORDER_PURPLE = "rgb(153, 102, 255)";
+  // const HAPPINESS_TRACKER_BORDER_YELLOW = "rgb(255, 205, 86)";
+  // const HAPPINESS_TRACKER_BORDER_GREEN = "rgb(75, 192, 192)";
 
-  return (
-    <>
-      {isLoading ? (
-        <div>loading...</div>
-      ) : (
-        <Bar options={options} data={data} />
-      )}
-    </>
-  );
-};
+  // const EMPTY_HAPPINESS_TRACKER_DATA = [0, 0, 0, 0, 0];
 
-const StatisticsPage = () => {
+  const { projectId } = useParams();
+  const [projectData, setProjectData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  // const [happinessTrackerData, setHappinessTrackerData] = useState(
+  //   EMPTY_HAPPINESS_TRACKER_DATA
+  // );
+
+  // const options = {
+  //   scales: {
+  //     y: {
+  //       ticks: {
+  //         stepSize: 1,
+  //       },
+  //     },
+  //   },
+
+  //   responsive: true,
+  //   plugins: {
+  //     legend: {
+  //       display: false,
+  //     },
+  //     title: {
+  //       display: true,
+  //       text: "Happiness Tracker",
+  //     },
+  //   },
+  // };
+
+  // const data = {
+  //   labels: ["Stressed", "Worried", "Neutral", "Comfortable", "Happy"],
+  //   datasets: [
+  //     {
+  //       label: "Happiness",
+  //       data: happinessTrackerData,
+  //       backgroundColor: [
+  //         HAPPINESS_TRACKER_BACKGROUND_RED,
+  //         HAPPINESS_TRACKER_BACKGROUND_ORANGE,
+  //         HAPPINESS_TRACKER_BACKGROUND_PURPLE,
+  //         HAPPINESS_TRACKER_BACKGROUND_YELLOW,
+  //         HAPPINESS_TRACKER_BACKGROUND_GREEN,
+  //       ],
+  //       borderColor: [
+  //         HAPPINESS_TRACKER_BORDER_RED,
+  //         HAPPINESS_TRACKER_BORDER_ORANGE,
+  //         HAPPINESS_TRACKER_BORDER_PURPLE,
+  //         HAPPINESS_TRACKER_BORDER_YELLOW,
+  //         HAPPINESS_TRACKER_BORDER_GREEN,
+  //       ],
+  //       borderWidth: 1,
+  //     },
+  //   ],
+  // };
+
+  // const loadProject = async () => {
+  //   try {
+  //     const response = await getProject(projectId!);
+  //     console.log(response);
+  //     // let tmpHappinessTrackerData = EMPTY_HAPPINESS_TRACKER_DATA;
+  //     // response.profiles.map(
+  //     //   (profile: IProfile) => ++tmpHappinessTrackerData[profile.happiness!]
+  //     // );
+  //     // setHappinessTrackerData(tmpHappinessTrackerData);
+  //     // setIsLoading(false);
+  //   } catch (err: any) {
+  //     console.log(err);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   loadProject();
+  //   // eslint-disable-next-line
+  // }, []);
+
+
   return (
     <StatisticsPageContainer>
-      <HappinessTrackerGraph />
+      <Header />
+      <SectionContainer>
+        <h2>Happiness tracker</h2>
+        <Divider />
+        <GraphWrapper>
+          <Pie data={data} />  
+        </GraphWrapper>
+      </SectionContainer>
+      <SectionContainer>
+        <h2>Collective busyness</h2>
+        <Divider />
+        {/* todo: insert column graph indicating percentages of happy, sad, mid, etc. */}
+      </SectionContainer>
+      <SectionContainer>
+        <h2>Status of tasks</h2>
+        <Divider />
+        {/* todo: insert table? for number of ... */}
+      </SectionContainer>
+      <Footer />
     </StatisticsPageContainer>
   );
 };
+
 export default StatisticsPage;
--- frontend/src/pages/StatisticsPage/style.ts
+++ frontend/src/pages/StatisticsPage/style.ts
@@ -1,7 +1,28 @@
 import { styled } from "@mui/system";
 
 export const StatisticsPageContainer = styled("div")`
-  width: 80%;
-  height: 80%;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  position: relative;
+  padding-bottom: 3rem;
 `;
 
+export const SectionContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  padding: 0 3rem;
+  margin-bottom: 1rem;
+  > h2 {
+    font-weight: normal;
+  }
+`; 
+
+export const GraphWrapper = styled('div')`
+  align-self: center;
+  position: relative;
+  height: 20rem;
+  width: 20rem;
+`;
